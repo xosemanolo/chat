555f19eaab16c2432f062298282cdcadbe3b1631
xosemanolo <xose.manolo.rodriguez@gmail.com>
2020-04-19 14:36:56 -0400

    add express code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// test
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+http.listen(PORT, () => {
+    console.log(`Chat Messenger listening on port: ${PORT}`)
+})
